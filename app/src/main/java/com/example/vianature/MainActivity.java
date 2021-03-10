7de1decd26f9895b6e8043eeda28fc94978a56c5
rrADISLAV <radislav.nedelchev@gmail.com>
2021-03-10 22:32:48 +0200

    Home screen - link to login page added

--- app/src/main/java/com/example/vianature/MainActivity.java
+++ app/src/main/java/com/example/vianature/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.vianature;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (TextView) findViewById(R.id.login);
+        login.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+               // startActivity(new Intent(this, Login.class));
+                break;
+        }
+    }
+
+
 }
