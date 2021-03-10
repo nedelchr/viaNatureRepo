be3cf01b377cdb92ad4a595efb4acdc50f2d39eb
rrADISLAV <radislav.nedelchev@gmail.com>
2021-03-11 00:54:17 +0200

    Registration validation added: - Error when name, email and password is empty - Error when email does not match with the email pattern - Error when the password does not meet the minimum requirement of at least 6 characters long

--- app/src/main/java/com/example/vianature/RegisterUser.java
+++ app/src/main/java/com/example/vianature/RegisterUser.java
@@ -2,13 +2,93 @@ package com.example.vianature;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextEmail, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.name);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logo:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(fullName.isEmpty()) {
+            editTextFullName.setError("Name is required!");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter proper email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.length()<6) {
+            editTextPassword.setError("Please provide at least 6 characters long password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+
     }
 }
