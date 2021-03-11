719ae59bb8d17bed6dcb007bd2e18c546a545ac5
rrADISLAV <radislav.nedelchev@gmail.com>
2021-03-11 13:09:48 +0200

    Logout button implemented - When the user logs in successfully the app redirect him to a page with its profile information(not created yet) and in this page there is a logout button. Whenever it is pressed the app will signout the user and direct him/her to the page where he/she can enter its credentials again and login

--- app/src/main/java/com/example/vianature/ProfileActivity.java
+++ app/src/main/java/com/example/vianature/ProfileActivity.java
@@ -2,13 +2,29 @@ package com.example.vianature;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logOut = (Button) findViewById(R.id.signOut);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, LoginUser.class));
+            }
+        });
     }
 }
