b040f098547d73e618f701dff6114fe3a8af78a4
rrADISLAV <radislav.nedelchev@gmail.com>
2021-03-11 12:16:17 +0200

    Login Authentication via email successfully integrated - When the user enter its credentials and the app checks with the database, if they match the user is redirected to its user profile, if not a excpetions will popup with missing/wrong data

--- app/src/main/java/com/example/vianature/LoginUser.java
+++ app/src/main/java/com/example/vianature/LoginUser.java
@@ -1,16 +1,33 @@
 package com.example.vianature;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginUser extends AppCompatActivity implements View.OnClickListener {
 
     private TextView register;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,16 @@ public class LoginUser extends AppCompatActivity implements View.OnClickListener
 
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -28,6 +55,51 @@ public class LoginUser extends AppCompatActivity implements View.OnClickListener
             case R.id.register:
                 startActivity(new Intent(this, RegisterUser.class));
                 break;
+            case R.id.signIn:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()<6) {
+            editTextPassword.setError("Password is less than 6 chars");
+            editTextPassword.requestFocus();
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        //red to user profile
+                        startActivity(new Intent(LoginUser.this, ProfileActivity.class));
+                    }else {
+                        Toast.makeText(LoginUser.this, "Failed to login. Please check credentials", Toast.LENGTH_LONG).show();
+                    }
+            }
+        });
     }
 }
